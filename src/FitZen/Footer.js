19cb4be0e9db32d37c94290a31afc09d9662fcac
Mohd Atfan <143632074+atfan7@users.noreply.github.com>
2025-10-26 17:52:34 +0530

    Change footer position from sticky to fixed

--- src/FitZen/Footer.js
+++ src/FitZen/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer
       className="container-fluid p-4 text-bg-dark text-center"
       style={{
-        position: 'sticky',
+        position: 'fixed',
         bottom: 0,
         left: 0,
         width: '100%',
